68afe7f51b21faac572106e1f94ac1143ea2cc93
Volodymyr Lyubinets <volodymyr@VLADMACBOOK.local>
2014-01-25 15:04:01 -0800

    Using 2 data sources

--- proj/app/views.py
+++ proj/app/views.py
@@ -2,65 +2,95 @@ import re
 import json
 import urllib2
 import requests
-
+import os
+import sys, traceback
+import string
+import random
+import math
 from flask import request, render_template, send_from_directory
 from app import app
 
-from pykml.factory import KML_ElementMaker as KML
-from pykml.factory import ATOM_ElementMaker as ATOM
-from pykml.factory import GX_ElementMaker as GX
-from lxml import etree
+def generate_circle_points(lng, lat):
+	res = ""
+	delta = 0.002
+
+	for i in xrange(360):
+		res += str(lng + (delta * math.cos( ( 2 * 3.14159 * i ) / 360 ))) + "," + str(lat + (delta * math.sin( ( 2 * 3.14159 * i ) / 360 ))) + ",500\n"
+	res += str(lng + (delta * math.cos(0))) + "," + str(lat + (delta * math.sin(0))) + ",500\n"
+
+	return res
+
+def generate_placemark(name, lng, lat):
+	circle_points = generate_circle_points(lng, lat)
+
+	plc = '''
+  <Folder>
+    <name>Place data</name>
+    <visibility>1</visibility>
+    <Placemark>
+      <name>circle</name>
+      <visibility>1</visibility>
+      <Style>
+        <geomColor>ff0000ff</geomColor>
+        <geomScale>1</geomScale>
+      </Style>
+	    <Style id="exampleBalloonStyle">
+			<BalloonStyle>
+			  <!-- a background color for the balloon -->
+			  <bgColor>ffffffbb</bgColor>
+			  <!-- styling of the balloon text -->
+			  <text><![CDATA[
+			  <b><font color="#CC0000" size="+3">%s</font></b>
+			  <br/><br/>
+			  <font face="Courier">%s</font>
+			  <br/><br/>
+			  Extra text that will appear in the description balloon
+			  <br/><br/>
+			  ]]></text>
+			</BalloonStyle>
+			 <geomColor>ff0000ff</geomColor>
+        <geomScale>1</geomScale>
+		  </Style>
+	<Polygon>
+	  <!-- specific to Polygon -->
+	  <name>BalloonStyle</name>
+	  <extrude>1</extrude>                       <!-- boolean -->
+	  <tessellate>0</tessellate>                 <!-- boolean -->
+	  <altitudeMode>relativeToGround</altitudeMode>
+	  <styleUrl>#exampleBalloonStyle</styleUrl>
+	  <outerBoundaryIs>
+		<LinearRing>
+		          <coordinates> 
+		          	%s
+					</coordinates>
+		</LinearRing>
+	  </outerBoundaryIs>
+	</Polygon>
+    </Placemark>
+  </Folder>
+	''' % (name, name, circle_points)
+
+	#placement = '''<Placemark><name>%s</name>
+    #  		<Point>
+    #    		<coordinates>%f,%f</coordinates>
+    #  		</Point>
+    # 	</Placemark>''' % (data.get("name"), data.get("lng"), data.get("lat"))
+	return plc
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for x in range(size))
 
 # Given the dictionaries, describing places, generated the kml file
 def generate_kml(places):
-	#doc = KML.kml(
-	#    KML.Document(
-	#        KML.Name("Awesome places"),
-	#        KML.Style(
-	#            KML.IconStyle(
-	#                KML.scale(1.2),
-	#                KML.Icon(
-	#                    KML.href("http://maps.google.com/mapfiles/kml/pal4/icon28.png")
-	#                ),
-	#            )
-	#        )
-	#    )
-	#)
-
-	#for data in places:
-	#	pm = KML.Placemark(
-   	#		KML.name(data.get("name")),
-   	#		KML.Point(KML.coordinates(str(data.get("lng")) + "," + str(data.get("lat"))))
-  	#	)
-  	#	doc.Document.append(pm)
-
-	#result = etree.tostring(doc, pretty_print=True)
-	#result.replace("placemark", "Placemark")
-	#result.replace("point", "Point")
-
-	result = '''<kml xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://www.opengis.net/kml/2.2">
-  <Document>
-    <Name>Awesome places</Name>
-    <Style>
-      <IconStyle>
-        <scale>1.2</scale>
-        <Icon>
-          <href>http://maps.google.com/mapfiles/kml/pal4/icon28.png</href>
-        </Icon>
-      </IconStyle>
-    </Style>'''
+	result = '''<?xml version="1.0" encoding="UTF-8"?>
+				<kml xmlns="http://www.opengis.net/kml/2.2"><Folder>'''
 
 	for data in places:
-		placement = '''<Placemark><name>%s</name>
-      		<Point>
-        		<coordinates>%f,%f</coordinates>
-      		</Point>
-    	</Placemark>''' % (data.get("name"), data.get("lng"), data.get("lat"))
-		result += placement
+		result += generate_placemark(data.get("name"), data.get("lng"), data.get("lat"))
 
-  	result += '''</Document></kml>'''
+  	result += '''</Folder></kml>'''
 
-	return result
+	return result.encode('utf-8')
 
 def get_foursqure(query, near):
 	assert query != None
@@ -87,8 +117,8 @@ def get_foursqure(query, near):
 			pass
 
 	return res
-	
-def get_google_places(query, near):
+
+def get_google(query, near):
 	assert query != None
 	assert near != None
 	API_KEY = "AIzaSyA8H80z37hBd_lLpowHKl73l2wP650VUEs"
@@ -106,22 +136,31 @@ def get_google_places(query, near):
 	return res
 
 
-@app.route('/generate', methods=['POST', 'GET'])
+@app.route('/generate', methods=['GET'])
 def generate():
+
 	query = request.args.get('query')
 	near = request.args.get('near')
+	
+	places_foursqaure = get_foursqure(query, near)
+	places_google = get_foursqure(query, near)
 
-	placesFoursquare = get_foursqure(query, near)
-	placesGoogle = get_google_places(query, near)
-	places = placesFoursquare + placesGoogle
-	kml = generate_kml(places)
+	places_combined = places_foursqaure
+	for place in places_google:
+		places_combined.append(place)
 
-	os.remove("app/static/x.kml")
-	f = open('app/static/x.kml', 'w+')
-	f.write(kml)
-	f.close()
+	kml = generate_kml(places_combined)
+	
+	id = id_generator()
+	f = open('app/static/x' + id + '.kml', 'w+')
 
-	return "static/x.kml" # OK kml
+	try:
+		f.write(kml)
+		f.close()
+	except:
+		return request.url_root + 'static/x' + id + '.kml'
+
+	return request.url_root + 'static/x' + id + '.kml' # OK (kml)
 
 @app.route('/')
 @app.route('/home')
