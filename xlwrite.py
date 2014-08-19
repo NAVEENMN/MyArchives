392ed9b2a20a48461d448ca86d65a440206d2e4e
Naveen Mysore <navimn1991@gmail.com>
2014-08-18 22:32:13 -0400

    second commit

--- xlwrite.py
+++ xlwrite.py
@@ -9,15 +9,18 @@ def setup_workbook():
 	p, q = find_boundries(rb)
 	print p
 	print q
-	'''
-	for x in range(0,10):
-		val = rb.sheet_by_index(0).cell(x,0).value
-		print val
-		answer = raw_input("enter: ")
-		wb.get_sheet(0).write(x+1,0,answer)
-	'''
-	os.system("rm sample.xls")
-	wb.save('sample.xls')
+	for x in range(1,p):
+		val = rb.sheet_by_index(0).cell(0,x).value
+		print "Input data for " + val
+		for y in range(1,q):
+			val = rb.sheet_by_index(0).cell(y,0).value
+			print val
+			answer = raw_input("enter: ")
+			wb.get_sheet(0).write(y,x,answer)
+		os.system("rm sample.xls")
+		print "Saving records"
+		wb.save('sample.xls')
+
 def find_boundries(rb):
 	sheet1 = rb.sheet_by_index(0)
 	xcount = 0
@@ -26,12 +29,13 @@ def find_boundries(rb):
 	while(val != "end"):
 		val = rb.sheet_by_index(0).cell(0,xcount).value
 		xcount = xcount + 1 
-		print xcount
+	xcount = xcount - 1
 	val = 0
 	while(val != "end"):
 		val = rb.sheet_by_index(0).cell(ycount,0).value
 		ycount = ycount + 1
-		print ycount
+	ycount = ycount - 1
+
 	return(xcount, ycount)	
 
 if __name__ == "__main__" :
