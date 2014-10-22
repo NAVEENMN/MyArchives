6dcfdf79509715ad4487885580fa89ad785ee2a9
Naveen Mysore <navimn1991@gmail.com>
2014-10-22 19:11:10 -0400

    with_parser

--- src/com/example/metster/Rend.java
+++ src/com/example/metster/Rend.java
@@ -1,6 +1,7 @@
 package com.example.metster;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.metster.Login.Map;
+import com.example.metster.ProfilelistActivity.account;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -23,6 +25,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Rend extends Activity {
 	private static final int CONTACT_PICKER_RESULT = 1001;
+	String server_response;
 	ArrayList<String> group_list = new ArrayList<String>();
 	public static class group{
 		static String curr_person;
@@ -52,6 +55,22 @@ public class Rend extends Activity {
 		EditText emailEntry = (EditText) findViewById(R.id.invite_email);
         group.curr_person = emailEntry.getText().toString();
         Log.w("requesting",group.curr_person);
+        get_this_person_loc(group.curr_person);
+	}
+	public void get_this_person_loc(String per_email){
+		new GetRestaurantAsyncTask().execute("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyCZQEuWjrNvrvPFzx6SQNxk_2xjtnGWvHE");
+		/*
+		try {
+	    	server_response = new RequestTask().execute("http://54.183.113.236/metster/getprofiledata.php", account.appkey, group.curr_person, "1","1","1", "1", "1"
+					, "1", "1", "1", "1", "1", "1" ).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
 	}
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
