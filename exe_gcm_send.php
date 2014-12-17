c2fe62d4735a35f8f6b459cea36e482e10252a09
Naveen Mysore <navimn1991@gmail.com>
2014-12-17 23:26:36 +0000

    facebookid

--- exe_gcm_send.php
+++ exe_gcm_send.php
@@ -1,7 +1,7 @@
 <?php
 $accountnumber = $_POST['appkey'];
-$to_contactnumber = $_POST['param2'];
+$to_facebookid = $_POST['param2'];
 $message = $_POST['param3'];
-$output = shell_exec('php send_gcm_message.php '.$accountnumber.' '.$to_contactnumber.' '.$message);
+$output = shell_exec('php send_gcm_message.php '.$accountnumber.' '.$to_facebookid.' '.$message);
 echo "$output";
 ?>
--- send_gcm_message.php
+++ send_gcm_message.php
@@ -1,12 +1,12 @@
 <?php
 include 'databaseauth.php';
 $from_accountnumber = $argv[1];//$_POST['appkey'];//who sent it
-$to_email_as_ref = $argv[2];//$_POST['param2'];//check to whom to send with email
+$to_facebookid = $argv[2];//$_POST['param2'];//check to whom to send with email
 $message = $argv[3];
 //$message = $_POST['param3'];
 //-------> getting to gcm refrence
 $result = mysql_query("SELECT * FROM details
- WHERE contact ='$to_email_as_ref'") or die(mysql_error());
+ WHERE facebookid ='$to_facebookid'") or die(mysql_error());
 $row = mysql_fetch_array( $result );
 if($row){
 $to_gcm = $row['gcm'];
