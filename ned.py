9f46c6b83dd3616e6c22219c1479c7f0a083c0df
Naveen Mysore <navimn1991@gmail.com>
2015-03-09 11:06:05 -0400

    all three

--- ned.py
+++ ned.py
@@ -1,10 +1,71 @@
-class edit_distance:
-	def __init__(self, stringa, stringb):
-		self.A = stringa
-		self.B = stringb
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+ 
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+ 
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+ 
+    return previous_row[-1]
+
+def lcs(a, b):
+    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
+    # row 0 and column 0 are initialized to 0 already
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i+1][j+1] = lengths[i][j] + 1
+            else:
+                lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
+    # read the substring out from the matrix
+    result = ""
+    x, y = len(a), len(b)
+    while x != 0 and y != 0:
+        if lengths[x][y] == lengths[x-1][y]:
+            x -= 1
+        elif lengths[x][y] == lengths[x][y-1]:
+            y -= 1
+        else:
+            assert a[x-1] == b[y-1]
+            result = a[x-1] + result
+            x -= 1
+            y -= 1
+    return result
+
+def lcsr(xstr, ystr):
+    """
+    >>> lcs('thisisatest', 'testing123testing')
+    'tsitest'
+    """
+    if not xstr or not ystr:
+        return ""
+    x, xs, y, ys = xstr[0], xstr[1:], ystr[0], ystr[1:]
+    if x == y:
+        return x + lcsr(xs, ys)
+    else:
+        return max(lcsr(xstr, ys), lcsr(xs, ystr), key=len)
 
 def main():
-	ed = edit_distance("Hello","World")
-
+	StringA = str(raw_input("Enter String A: "))
+	StringB = str(raw_input("Enter String B: "))
+	ed = float( levenshtein(StringA, StringB) )
+	total_length = float( len(StringA) + len(StringB) )
+	normalized_edit_distance = (total_length - ed)/(total_length)
+	print "Edit Distance: ", ed
+	print "Normalized Edit Distance: ", normalized_edit_distance
+	LCS = lcs(StringA, StringB)
+	LCSR = lcsr(StringA, StringB)
+	print "Longest Common Subsequence: ", LCS
+	print "Longest Common Subsequence recursive: ", LCSR
 if __name__ == "__main__":
 	main()
