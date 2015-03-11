a6e2e5c0cc8a708af0a2e5a7ba74ad465908da68
Naveen Mysore <navimn1991@gmail.com>
2015-03-10 23:34:09 -0400

    pre_final

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+# Implementation of Longest common subsequence by calculating edit distances
 # Author : Naveen Mysore
+# UNCC ID : 800812071
+
+This the project done as part of my course requirement for Algorithms and data structures class at UNCC
+Spring 2015
+
+This is the implementation of finding longest common subsequence by
+
+• Normalized Edit Distance Algorithm
+• LCS Algorithm – stores the entire table
+• LCS Algorithm – linear memory recursive version
+
+1. Implementation of the algorithm for normalized edit distance.
+This function takes two sequences and computes the normalized edit distance between them.
+This function uses only two rows so that the table is not stored in memory.
+
+functions used : 
+
+2. Implementation of the LCS algorithm that stores the entire table.
+This function computes and returns the longest common subsequence of two sequences. This
+function is implemented using the basic dynamic programming algorithm that stores the entire table in memory.
+
+3. Implementation of the LCS algorithm that uses linear memory.
+This function computes and returns the longest common subsequence of two sequences. This
+function is implemented using the recursive linear memory algorithm.
--- ned.py
+++ ned.py
@@ -1,3 +1,6 @@
+# Author : Naveen Mysore
+# UNCC ID: 800812071
+
 #Copyright 2015. Naveen Mysore, Inc. All Rights Reserved.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import sys
 
 '''
 Integer edit_distance(String, String)
@@ -62,10 +67,12 @@ return 	: String - longest common subsequence
 '''
 def lcs(a, b):
 	Matrix = [[0 for j in range(len(b)+1)]  for i in range(len(a)+1)] # row 0 and column 0 are initialized to 0
+	#setting up the costs for empty string insertions and deletions
 	for y in range(0, len(b)+1):
 		Matrix[0][y] = y
 	for x in range(0, len(a)+1):
 		Matrix[x][0] = x
+	#computing the entire matrix
 	for i, x in enumerate(a, 1):
         	for j, y in enumerate(b, 1):
 			insert_cost = Matrix[i-1][j]+1
@@ -76,7 +83,6 @@ def lcs(a, b):
 				substitute_cost = Matrix[i-1][j-1]+1
 			cell_cost = min(insert_cost, delete_cost, substitute_cost)
                 	Matrix[i][j] = cell_cost
-
 	# uncomment the below section to see the table
 	'''
 	row = list()
@@ -86,7 +92,7 @@ def lcs(a, b):
 		print row
 		del row[:]
 	'''		
-	# backtrack the subsequence from the matrix
+	# backtracking the subsequence from the matrix
 	result = ""
 	x, y = len(a), len(b)
 	while x != 0 and y != 0:
@@ -104,9 +110,17 @@ def lcs(a, b):
 				
 	return result[::-1]#reversing the string
 
+'''
+    Void lcs_recursive(String, String, List)
+    @desc : This function takes string inputs a and b and a list which holds the result
+    @param1 : String - input A
+    @param2 : String - input B
+    @param3 : List - longest common subsequence in form of a list
+    return 	: void
+'''
 def lcs_recursive(a, b, LCSR):
-	print "inputs are", a
-	print b
+    # If either of the input is a character then compare it
+    # against all characters of other string
 	if len(a) == 1:
 		for x in range(0, len(b)):
 			if a[0] == b[x]:
@@ -120,27 +134,11 @@ def lcs_recursive(a, b, LCSR):
 		#computing the middle row
 		middle = (len(a)/2)-1
 		if middle == 0:
-			middle = 1
-		print "middle is : ", middle
-		Matrix = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)] # row 0 and column 0 are initialized to 0
-		for y in range(0, len(b)+1):
-			Matrix[0][y] = y
-		for x in range(0, len(a)+1):
-			Matrix[x][0] = x
-		for i, x in enumerate(a, 1):
-        		for j, y in enumerate(b, 1):
-				if x <= middle: #first half of matrix
-					insert_cost = Matrix[i-1][j]+1
-					delete_cost = Matrix[i][j-1]+1
-					if x == y:
-						substitute_cost = Matrix[i-1][j-1]
-					else:
-						substitute_cost = Matrix[i-1][j-1]+1
-					cell_cost = min(insert_cost, delete_cost, substitute_cost)
-                			Matrix[i][j] = cell_cost
-		#Top Matrix compute		
+            		middle = 1 # if middle become zero then pick next row
+		#Top Matrix and bottom Matrix setting up the matrix
 		TopMatrix = [[0 for j in range(len(b)+1)] for i in range(0,middle+1)] # row 0 and column 0 are initialized to 0
 		BottomMatrix = [[0 for j in range(len(b)+1)] for i in range(0,len(a)-middle+1)] # row 0 and column 0 are initialized to 0
+		#Top Matrix and Bottom Matrix setting up the costs for empty string insertions and deletions
 		for y in range(0, len(b)+1):
 			TopMatrix[0][y] = y
 		for x in range(0, middle+1):
@@ -149,6 +147,7 @@ def lcs_recursive(a, b, LCSR):
 			BottomMatrix[0][y] = y
 		for x in range(0, len(a)-middle+1):
 			BottomMatrix[x][0] = x
+		#Computing the TopMatrix
 		for i, x in enumerate(a, 1):
         		for j, y in enumerate(b, 1):
 				if i <= middle: #first half of matrix
@@ -164,7 +163,7 @@ def lcs_recursive(a, b, LCSR):
 		temp = a[middle:]
 		c = temp[::-1]
 		d = b[::-1]
-		print BottomMatrix
+		#Computing the BottomMatrix
 		for i, x in enumerate(c, 1):
         		for j, y in enumerate(d, 1):
 				insert_cost = BottomMatrix[i-1][j]+1
@@ -176,7 +175,8 @@ def lcs_recursive(a, b, LCSR):
 				cell_cost = min(insert_cost, delete_cost, substitute_cost)
                 		BottomMatrix[i][j] = cell_cost
 
-		# uncomment the below section to see the table
+		# uncomment the below section to see the Top Matrix
+		'''
 		row = list()
 		for x in range(0, middle+1):
 			for y in range(0, len(b)+1):
@@ -184,20 +184,19 @@ def lcs_recursive(a, b, LCSR):
 			print row
 			del row[:]
 		print " "
-		# uncomment the below section to see the table
+		'''
+		# uncomment the below section to see the Bottom Matrix
+		'''
 		row = list()
 		for x in range(0, len(c)+1):
 			for y in range(0, len(b)+1):
 				row.append(BottomMatrix[x][y])
 			print row
 			del row[:]
-
-		f_row = TopMatrix[middle]
-		print "bug ", len(BottomMatrix) - 1
-		t = BottomMatrix[len(BottomMatrix) - 1]
-		b_row = t[::-1]
-		print "forward : ", f_row
-		print "backward: ", b_row
+		'''
+		f_row = TopMatrix[middle] # This is the forward middle row
+		t = BottomMatrix[len(BottomMatrix) - 1] 
+		b_row = t[::-1] # This is the Reverse middle row
 
 		#finding mimimum edit distance
 		pool = list()
@@ -208,25 +207,41 @@ def lcs_recursive(a, b, LCSR):
 		y_front = ''.join(a[0:middle]) # 0 to middle
 		x_back = ''.join(b[end:])
 		y_back = ''.join(a[middle:])
-		print "x_front",x_front, len(x_front)
-		print "y_front",y_front, len(y_front)
-		print "x_back",x_back, len(x_back)
-		print "y_back",y_back, len(y_back)
+		#x_front, y_front, x_back, y_back split the Whole matrix to two sub matrices
+		#They also hold the strings which we will use inside recursive call
 		lcs_recursive(x_front, y_front, LCSR)
 		lcs_recursive(x_back, y_back, LCSR)	
-		
-def main():
-	StringA = str(raw_input("Enter String A: "))
-	StringB = str(raw_input("Enter String B: "))
+'''
+    Void main(String, String)
+    @desc : This is the main function takes file names as string inputs 
+    @param1 : String - file a
+    @param2 : String - file b
+    return 	: void
+'''	
+def main(filea, fileb):
+	#reading input files
+	fa = open(filea)
+	StringA = str(fa.read())
+	fa.close()
+	fb = open(fileb)
+	StringB = str(fb.read())
+	fb.close()
+	print "String A : ", StringA
+	print "String B : ", StringB
+	
 	LCSR = list()
-	ed = float( edit_distance(StringA, StringB) )
+	ed = float( edit_distance(StringA, StringB) ) # computing edit distance
 	total_length = float( len(StringA) + len(StringB) )
-	normalized_edit_distance = (total_length - ed)/(total_length)
-	print "Edit Distance: ", ed
-	print "Normalized Edit Distance: ", normalized_edit_distance
+	normalized_edit_distance = (total_length - ed)/(total_length) # computing normalized edit distance
+	print "Normalized Edit Distance: - uses only two rows: ", normalized_edit_distance
 	LCS = lcs(StringA, StringB)
 	lcs_recursive(StringA, StringB, LCSR)
-	print "Longest Common Subsequence: ", LCS
-	print "Longest Common Subsequence recursive: ", ''.join(LCSR)
+	print "LCS - Stores the entire table: ", LCS
+	print "LCS - linear memory recursive version: ", ''.join(LCSR)
+
 if __name__ == "__main__":
-	main()
+	if(len(sys.argv) != 3):
+		print "Usage: python ned.py [filea.txt] [fileb.txt]"
+	else:
+		arguments = sys.argv[1:]
+		main(arguments[0], arguments[1])
